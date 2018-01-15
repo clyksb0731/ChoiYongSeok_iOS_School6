6fceab50014b88b6125b6d4506c9bfc5063d764b
Yongseok Choi <clyksb0731@gmail.com>
2018-01-16 00:15:45 +0900

    complete practice functions on PlayGround

--- Practices/MyPlaygroundSam.playground/Contents.swift
+++ Practices/MyPlaygroundSam.playground/Contents.swift
@@ -13,7 +13,7 @@ func printAge(Integer age: Int) {
 }
 
 // 이름과 주소를 입력받아 자기소개글을 프린트 하는 함수
-func printPR(name name: String, address address: String){
+func printPR(name: String, address: String){
     print("저의 이름은 \(name)입니다. 그리고 저는 \(address)에 살고 있습니다.")
 }
 
@@ -52,4 +52,30 @@ func divisionCalculator(first num1: Int, second num2: Int) -> Double {
         return 0
     }
 }
+
+// 부모님과 내 나이를 입력 후 그 나이차를 프린트 하는 함수
+func ageDiff(momAge mom: Int, myAge me: Int) {
+    print("어머니의 나이는 \(mom)세이고 내 나이는 \(me)살이므로 우리의 나이차이는 \(mom - me)살 입니다.")
+}
+
+// 시험점수 여러개를 입력받아서 (4개 이상) 평균을 반환해주는 함수
+func average(eng: Int, math: Int, literature: Int, science: Int) -> Double {
+    return Double(eng + math + literature + science) / 4
+}
+
+// 정수를 하나 입력 받아서 실수로 변환 후 반환하는 함수
+func changeDataType(integer num: Int) -> Double {
+    return Double(num)
+}
+
+// 정수를 두개를 입력받아 두수를 합친 수를 출력하는 함수
+func sum(integer1 num1: Int, integer2 num2: Int) -> Int {
+    return num1 + num2
+}
+
+// 실수를 하나 입력받아서 소수점 첫 번째 자리에서 반올림 후 정수를 반환해주는 함수
+func round(realNumber num: Double) -> Int {
+    return Int(num)
+}
+
 
