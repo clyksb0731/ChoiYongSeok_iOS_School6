379a1df00e06e5a97f110a3f02161bebdd7ba1ba
Yongseok Choi <clyksb0731@gmail.com>
2018-02-07 02:15:06 +0900

    Update project 'UI_customizedView' for implementation of arrangement into table type with views

--- Practices/UI_customizedView/UI_customizedView/CustomizedView.swift
+++ Practices/UI_customizedView/UI_customizedView/CustomizedView.swift
@@ -17,6 +17,36 @@ class CustomizedView: UIView {
     var lowerLabel: UILabel?
     var coverBtn: UIButton?
     
+    var upperLabelText: String? {
+        get {
+            guard let upperLabel = upperLabel else { return nil }
+            return upperLabel.text
+        }
+        set {
+            upperLabel?.text = newValue
+        }
+    }
+    
+    var lowerLabelText: String? {
+        get {
+            guard let lowerLabel = lowerLabel else { return nil }
+            return lowerLabel.text
+        }
+        set {
+            lowerLabel?.text = newValue
+        }
+    }
+    
+//    var tagIndex: Int = 0 {
+//        willSet {
+//            guard let coverBtn = coverBtn else { return }
+//            
+//            coverBtn.tag = newValue
+//        }
+//    }
+    
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -34,7 +64,8 @@ class CustomizedView: UIView {
         layoutView()
     }
     
-    private func createView() {
+    
+    func createView() {
         bottomView = UIView()
         self.addSubview(bottomView!)
         
@@ -45,31 +76,47 @@ class CustomizedView: UIView {
         
         upperLabel = UILabel()
         upperLabel!.textAlignment = .center
-        upperLabel!.font = UIFont.systemFont(ofSize: 80)
+//        upperLabel!.font = UIFont.systemFont(ofSize: 80)
+        upperLabel!.textColor = .green
         bottomView!.addSubview(upperLabel!)
         
         lowerLabel = UILabel()
         lowerLabel!.textAlignment = .right
-        lowerLabel!.font = UIFont.systemFont(ofSize: 40)
+//        lowerLabel!.font = UIFont.systemFont(ofSize: 40)
+        lowerLabel!.textColor = .blue
         bottomView!.addSubview(lowerLabel!)
         
         coverBtn = UIButton()
+        coverBtn!.setTitle("임시번호", for: .normal) ///////////// 테스트용
+        coverBtn!.setTitleColor(.clear, for: .normal) //////////// 테스트용
         coverBtn!.backgroundColor = .clear
         bottomView!.addSubview(coverBtn!)
     }
     
-    private func layoutView() {
+    func setLabelsFontSize(first uLbFont: CGFloat, second lLbFont: CGFloat) {
+        guard let upperLabel = upperLabel, let lowerLabel = lowerLabel else { return }
+        upperLabel.font = UIFont.systemFont(ofSize: uLbFont)
+        lowerLabel.font = UIFont.systemFont(ofSize: lLbFont)
+    }
+    
+    func layoutView() {
         guard let bottomView = bottomView, let imgView = imgView, let upperLabel = upperLabel, let lowerLabel = lowerLabel, let coverBtn = coverBtn else { return }
         
         bottomView.frame = self.bounds
         
         imgView.frame = self.bounds
         
-        upperLabel.frame = CGRect(x: 0, y: 0, width: bottomView.bounds.size.width, height: bottomView.bounds.size.height * 2 / 3)
+        upperLabel.frame = CGRect(x: self.bounds.size.width / 10, y: 0, width: self.bounds.size.width - self.bounds.size.width / 5, height: self.bounds.size.height * 2 / 5)
         
-        lowerLabel.frame = CGRect(x: 0, y: upperLabel.bounds.size.height, width: bottomView.bounds.size.width, height: bottomView.bounds.size.height * 1 / 3)
+        lowerLabel.frame = CGRect(x: self.bounds.size.width / 10, y: self.bounds.size.height * 4 / 5, width: self.bounds.size.width - self.bounds.size.width / 5, height: self.bounds.size.height * 1 / 5)
         
         coverBtn.frame = self.bounds
+    }
+    
+    func setButtonTag(number num: Int) {
+        guard let coverBtn = coverBtn else { return }
+        
+        coverBtn.tag = num
     }
     
     func addTarget(_ target: Any?, action: Selector, for controlEvents: UIControlEvents) {
--- Practices/UI_customizedView/UI_customizedView/ViewController.swift
+++ Practices/UI_customizedView/UI_customizedView/ViewController.swift
@@ -9,22 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var viewList: [CustomizedView]?
+    var basicFrame: UIView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        var tmpView: CustomizedView = CustomizedView()
-        tmpView.frame = CGRect(x: 30, y: 30, width: 200, height: 200)
-        tmpView.setUpView()
-        tmpView.upperLabel?.text = "위"
-        tmpView.lowerLabel?.text = "아래"
-        tmpView.addTarget(self, action: #selector(self.tmpAction(_:)), for: .touchUpInside)
-        self.view.addSubview(tmpView)
+//        var tmpView: CustomizedView = CustomizedView()
+//        tmpView.frame = CGRect(x: 30, y: 30, width: 200, height: 200)
+//        tmpView.setUpView()
+//        tmpView.upperLabel?.text = "위"
+//        tmpView.lowerLabel?.text = "아래"
+//        tmpView.addTarget(self, action: #selector(self.tmpAction(_:)), for: .touchUpInside)
+//        self.view.addSubview(tmpView)
+        createViews(number: 5)
+        layoutCustomViews(viewList: viewList, number: 3, width: 115)
+        
+    }
+    
+    func createViews(number num: Int) {
+        viewList = []
+        
+        for count in 0..<num {
+            let tmpCustomView: CustomizedView = CustomizedView()
+            tmpCustomView.createView()  // 구성 view 생성
+            tmpCustomView.upperLabelText = String(-1 * (count - num))
+            tmpCustomView.lowerLabelText = String(num + 1)
+//            tmpCustomView.tagIndex = -1 * (count - num)
+            tmpCustomView.setButtonTag(number: -1 * (count - num))
+            tmpCustomView.setLabelsFontSize(first: 30, second: 10)
+            tmpCustomView.addTarget(self, action: #selector(tmpAction(_:)), for: .touchUpInside)
+            viewList?.append(tmpCustomView)
+        }
+    }
+    
+    func layoutCustomViews(viewList: [CustomizedView]?, number num: Int, width: CGFloat) {
+        guard let viewList = viewList else { return }
+        
+        basicFrame = UIView(frame: CGRect(x: 10, y: 20, width: self.view.bounds.size.width, height: self.view.bounds.size.height - 20))
+        
+        self.view.addSubview(basicFrame!)
+        
+        for index in 0..<viewList.count {
+            let row = index / num
+            let col = index % num
+            
+            viewList[index].frame = CGRect(x: (10 + width) * CGFloat(col), y: width * CGFloat(row), width: width, height: width)
+            viewList[index].layoutView()
+            basicFrame?.addSubview(viewList[index])
+        }
+        
     }
     
     @objc func tmpAction(_ sender: UIButton) {
-        print("Action 성공")
+        print("\(sender.tag)", "*****", "\(sender.title(for: .normal)!)")
     }
 
     override func didReceiveMemoryWarning() {
