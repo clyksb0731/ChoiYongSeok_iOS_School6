58773ace09592b9c59b849fc3a887ab20c1dc160
Yongseok Choi <clyksb0731@gmail.com>
2018-03-16 17:16:56 +0900

    Create two new projects realted to how to add external frameworks

---
+++ Practices/Carthage_Sample/Carthage_Sample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Carthage_Sample
+//
+//  Created by 최용석 on 2018. 3. 16..
+//  Copyright © 2018년 Yongseok Choi. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Practices/CocoaPod_Sample/CocoaPod_Sample/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  CocoaPod_Sample
+//
+//  Created by 최용석 on 2018. 3. 16..
+//  Copyright © 2018년 Yongseok Choi. All rights reserved.
+//
+
+import UIKit
+import Then
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var tmp = UILabel()
+        tmp.then(<#T##block: (UILabel) throws -> Void##(UILabel) throws -> Void#>)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
